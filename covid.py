f1c92dcd4ffede0aecb42d46ee98bdd25eade37b
Dalton Maahs <dalton.maahs@yourbind.com>
2020-10-08 19:06:37 -0500

    graphs

--- covid.py
+++ covid.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import matplotlib as mpl
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 
@@ -11,7 +12,7 @@ def leading_digit(number):
     try:
         while number >= 10 or number <= -10:
             number = number / 10
-        return int(math.floor(number))
+        return int(math.floor(abs(number)))
     except ValueError:
         return np.nan # -1 means no data was reported for this state on this day
 
@@ -19,14 +20,26 @@ def leading_digit(number):
 def process_column(col, fn):
     return [fn(el) for el in col]
 
+def logRecip(number):
+    return 0-math.log(1 / ( number + 1 ))
+
 def analyse(data_, column):
     data["leading_digit"] = process_column(data[column], leading_digit)
     freq = pd.DataFrame()
 
     freq["count"] = data["leading_digit"].value_counts(dropna=True)
-    freq["freq"] = freq["count"] / freq["count"].sum()
     freq.index = process_column(freq.index, int)
+    freq = freq.drop(0)
+    freq["freq"] = freq["count"] / freq["count"].sum()
+    freq["log_freq"] = process_column(freq.freq, logRecip)
     return (freq, freq["count"].sum())
+
+def graph(freq_table, col):
+    plt.bar(freq_table.index, freq_table.freq)
+    plt.title(col)
+    plt.xlabel("digits")
+    plt.ylabel("freq")
+    plt.savefig(f"{col}_bar.png")
 
 if __name__ == "__main__":
     data = pd.read_csv("all-states-history.csv")
@@ -38,3 +51,4 @@ if __name__ == "__main__":
             print(freq.shape)
             print(freq)
             print("=================\n")
+            graph(freq, col)
