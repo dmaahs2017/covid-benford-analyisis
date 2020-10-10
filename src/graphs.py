e13c892c66f144f4ba0d62beff145c8dae59438e
Dalton Maahs <dalton.maahs@yourbind.com>
2020-10-10 13:41:14 -0500

    display y value

--- src/graphs.py
+++ src/graphs.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import utils
 import os
 import math
+import utils
 
 def display_freq(freq_tables, filter=""):
     for (table_name, table) in freq_tables:
@@ -16,6 +16,16 @@ def display_freq(freq_tables, filter=""):
         print("===============================================================")
         print("")
 
+def autolabel(rects, ax):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                round(height, 2),
+                ha='center', va='bottom')
+
 def graph_freq(freq_tables, title="Frequency of Digit Plots", show=False, export=True, export_path=".", export_to="digit"):
     rows = int(math.sqrt(len(freq_tables)))
     cols = math.ceil(len(freq_tables) / rows)
@@ -25,23 +35,24 @@ def graph_freq(freq_tables, title="Frequency of Digit Plots", show=False, export
 
     try:
         for ((table_name, table), ax) in zip(freq_tables, utils.flatten(axs)):
-            ax.bar(table.digit, table.freq)
+            bar = ax.bar(table.digit, table.freq)
             ax.set_title(table_name)
             ax.set_xlabel("digits")
             ax.set_ylabel("freq")
             ax.label_outer()
             ax.set_xticks(np.arange(1, 10, step=1))
-            ax.set_yticks(np.arange(0, .4, step=.05))
+            ax.set_ylim([0,1])
+            autolabel(bar, ax)
     except TypeError: # axs is just one plot
         for (table_name, table) in freq_tables:
-            axs.bar(table.digit, table.freq)
+            bar = axs.bar(table.digit, table.freq)
             axs.set_title(table_name)
             axs.set_xlabel("digits")
             axs.set_ylabel("freq")
             axs.label_outer()
             axs.set_xticks(np.arange(1, 10, step=1))
-            axs.set_yticks(np.arange(0, .4, step=.05))
-
+            axs.set_ylim([0,1])
+            autolabel(bar, axs)
 
     if export:
         try:
--- src/main.py
+++ src/main.py
@@ -2,62 +2,99 @@ import pandas as pd
 import graphs
 import analysis as an
 import fire
+import utils
 
-data = pd.read_csv("../data/all-states-history.csv")
-all_states = data["state"].unique()
-default_columns = ["positive", "death", "deathIncrease", "positiveCasesViral", "deathConfirmed", "deathProbable"]
 
 class CovidDataExplorer(object):
     """The various clis for interacting with the covid data"""
 
+    def __init__(self):
+        self._data = pd.read_csv("../data/all-states-history.csv")
+        self._all_states = self._data["state"].unique()
+        self._default_columns = ["positive", "death", "deathIncrease", "positiveCasesViral", "deathConfirmed", "deathProbable"]
+
+
+    def set_default_columns(self, cols):
+        self._default_columns = cols
+
     def view_default_columns(self):
         """View the default columns used in data analysis"""
-        print(default_columns)
+        print(self._default_columns)
 
     def list_columns(self):
         """Lists the columns in the covid data"""
-        print(data.dtypes)
+        print(self._data.dtypes)
 
     def list_states(self):
         """Lists the states in the covid data"""
-        for s in all_states:
+        for s in self._all_states:
             print(s)
 
-    def graph_usa(self, cols=default_columns, show=True, export=False):
+    def graph_usa(self, cols=[], show=True, export=False):
         """Graphs the specified list of columns"""
-        freq_data = [an.analyse(data, col) for col in cols]
+        if len(cols) == 0:
+            cols = self._default_columns
+        freq_data = [an.analyse(self._data, col) for col in cols]
         graphs.graph_freq(freq_data, title="Frequency of Digits for US", export=export,  show=show, export_path="USA")
 
-    def graph_state(self, state, cols=default_columns, show=True, export=False):
+    def graph_state(self, state, cols=[], show=True, export=False):
         """Graph state data for specified or default columns"""
-        state_data=data[data["state"] == state]
+        if len(cols) == 0:
+            cols = self._default_columns
+
+        state_data=self._data[self._data["state"] == state]
         state_freq_data = [an.analyse(state_data, col) for col in cols]
         graphs.graph_freq(state_freq_data, title=f"Frequency of Digits for {state}", export=export, show=show,  export_path=state)
 
-    def graph_states(self, states=all_states, cols=default_columns, show=False, export=True):
+    def graph_states(self, states=[], cols=[], show=True, export=False):
         """Graph data for multiple states. Default graphs all, other wise specify states list and cols list"""
+        if len(cols) == 0:
+            cols = self._default_columns
+        if len(states) == 0:
+            states = self._all_states
         for state in states:
             self.graph_state(state, cols, show, export)
 
-    def display_usa(self, cols=default_columns):
+    def display_usa(self, cols=[]):
         """Display USA data for specified or default columns"""
-        freq_data = [an.analyse(data, col) for col in cols]
+        if len(cols) == 0:
+            cols = self._default_columns
+        freq_data = [an.analyse(self._data, col) for col in cols]
         graphs.display_freq(freq_data)
 
-    def display_state(self, state, cols=default_columns):
+    def display_state(self, state, cols=[]):
         """Display state data for specified or default columns"""
-        state_data=data[data["state"] == state]
+        if len(cols) == 0:
+            cols = self._default_columns
+        state_data=self._data[self._data["state"] == state]
         state_freq_data = [an.analyse(state_data, col) for col in cols]
         graphs.display_freq(state_freq_data, filter=f"state: {state}")
 
-    def display_states(self, states=all_states, cols=default_columns):
+    def display_states(self, states=[], cols=[]):
         """Display data for all states, or specified states."""
+        if len(cols) == 0:
+            cols = self._default_columns
+        if len(states) == 0:
+            states = self._all_states
         for state in states:
             self.display_state(state, cols)
 
 
 def main():
-    fire.Fire(CovidDataExplorer(), name='cde')
+    cde = CovidDataExplorer()
+    commands = {
+        'defaultCols': cde.view_default_columns,
+        'listCols': cde.list_columns,
+        'setDefaultCols': cde.set_default_columns,
+        'listStates': cde.list_states,
+        'graphUsaCumu': cde.graph_usa,
+        'graphState': cde.graph_state,
+        'graphStates': cde.graph_states,
+        'displayUsaCumu': cde.display_usa,
+        'displayState': cde.display_state,
+        'displayStates': cde.display_states,
+    }
+    fire.Fire(commands)
 
 if __name__ == "__main__":
     main()
